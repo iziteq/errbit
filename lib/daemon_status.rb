0d0bc714f2aa37c3228ae974b857eb8b26f8b38a
Andrew <andrei.potapkin@iziteq.com>
2016-04-18 16:10:03 +0200

    Changed status endpoint logic

--- lib/daemon_status.rb
+++ lib/daemon_status.rb
@@ -4,7 +4,7 @@ require 'redis'
 
 class DaemonStatus
 
-  QUEUE_LIMIT         = 1000
+  QUEUE_LIMIT         = 30
   COMMANDSTATS_LIMIT = 1_000_000_000
 
   include Singleton
@@ -19,33 +19,34 @@ class DaemonStatus
 
   def alive?
     current_lpop_count = get_lpop_count
+
+    # It's unlikely that we'll reach this limit
+    # Suppose we've done it, then fail status endpoint and restart errbit manually
     return false if current_lpop_count >= COMMANDSTATS_LIMIT
 
-    if !is_first_run? && (!queue_clearing?(current_lpop_count) || queue_overflow?)
-      @prev_lpop_count   = current_lpop_count
-      false
-    else
-      @prev_lpop_count   = current_lpop_count
-      true
-    end
+    alive = first_run? || daemon_is_working?(current_lpop_count) || queue_is_empty?
+
+    # we have to increase througput of the queue processor
+    # so, notify about it by failing
+    alive = false unless queue_has_allowed_length?
+    
+    @prev_lpop_count = current_lpop_count
+    alive
   end
 
   private
 
-    def reset_stat
-      @redis.info(:resetstat)
-      @prev_lpop_count = nil
+    def queue_has_allowed_length?
+      queue_size =  redis.llen(redis_config['key'])
+      queue_size < QUEUE_LIMIT
     end
 
-    def queue_size
-      redis.llen(redis_config['key'])
+    def queue_is_empty?
+      redis.llen(redis_config['key']) == 0
     end
 
-    def queue_overflow?
-      queue_size >= QUEUE_LIMIT
-    end
-
-    def queue_clearing?(current_lpop_count)
+    def daemon_is_working?(current_lpop_count)
+      # if lpop count is increasing, we consider that the daemon does it
       @prev_lpop_count < current_lpop_count
     end
 
@@ -53,7 +54,7 @@ class DaemonStatus
       redis.info(:commandstats)['lpop']['calls'].to_i
     end
 
-    def is_first_run?
+    def first_run?
       @prev_lpop_count.nil?
     end
 
